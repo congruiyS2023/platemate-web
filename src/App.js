224c4c973fcb80055380e20d4712d2a713593bc8
“Lisa <liyangc2@andrew.cmu.edu>
2023-11-30 17:36:05 -0800

    add navibar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ConfigProvider } from "antd";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 import theme from "./assets/theme.json";
 import "./App.css";
@@ -9,14 +10,17 @@ import StyleGuide from "./routes/StyleGuide";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <ConfigProvider theme={theme}>
-          <Routes>
-            <Route path="/" element=<Home />></Route>
-            <Route path="/style-guide" element={<StyleGuide />} />
-          </Routes>
-        </ConfigProvider>
-      </div>
+      <ConfigProvider theme={theme}>
+        <div className="flex flex-col h-screen">
+          <div className="flex-grow overflow-auto pb-20">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/style-guide" element={<StyleGuide />} />
+            </Routes>
+          </div>
+          <Navbar />
+        </div>
+      </ConfigProvider>
     </Router>
   );
 }
